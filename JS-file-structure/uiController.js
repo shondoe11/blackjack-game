b06450900ea5755920295c90d0acc8a12ea18da6
Shawn Tan <shawnhadritan@gmail.com>
2024-12-25 15:56:16 +0800

    uiController.js:step4:leaderboard-function&export

--- JS-file-structure/uiController.js
+++ JS-file-structure/uiController.js
@@ -1,14 +1,31 @@
 /*-------------- Constants -------------*/
 
+// NA for now
 
 /*---------- Variables (state) ---------*/
 
+// NA for now
 
 /*----- Cached Element References  -----*/
 
+const leaderboardList = document.getElementById('leaderboardList');
 
 /*-------------- Functions -------------*/
 
+// render dynamic leaderboard
+function renderLeaderboard(leaderboard) {
+    leaderboardList.innerHTML = ''; // clear current leaderboard state first
+    leaderboard.forEach(player => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${player.name}: $${player.money}`;
+        leaderboardList.appendChild(listItem);
+    });
+}
 
 /*----------- Event Listeners ----------*/
 
+// NA for now
+
+/*--------------- Exports --------------*/
+
+export {renderLeaderboard};
